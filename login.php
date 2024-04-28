ba99efdca7efca4fea81b1b8e663a70a6bc4981d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 16:52:35 +0100

    Updated the login ui

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php require('meta-head.php'); ?>
+    <?php require ('meta-head.php'); ?>
     <title>Login | Black Cofee</title>
 </head>
+
 <body>
 
     <div class="bg-gray-200 w-full min-h-[100vh] flex justify-center items-center">
@@ -12,28 +14,36 @@
             <div class="mt-1 text-gray-500">Please sign-in to continue</div>
             <?php
 
-                if(isset($_POST['email'],$_POST['pswd'])){
-                    $email = $_POST['email'];
-                    $pswd = $_POST['pswd'];
-                    echo "pass";
+
+            if (isset($_POST['emailNname']) && isset($_POST['pswd'])) {
+                $emailNname = $_POST['emailNname'];
+                $pswd = $_POST['pswd'];
+                if ($emailNname !== "admin@gmail.com" || $pswd !== "admin") {
+                    ?>
+                    <div class="rounded p-5 bg-red-200 text-red-600 border-[1px] border-red-600 my-2">Invalid login. (Use the default values)</div>
+                <?php
                 }
+            }
 
             ?>
-            <form class="mt-2" method="POST" action=".">
+            <form class="mt-2" method="POST" action="/login">
                 <div class="mb-2">
-                    <label class="mt-1 text-gray-500">Email or Username</label>
-                    <input placeholder="johndoe@gmail.com" name="email" class="p-2 w-full outline-none border-2 border-gray-200 rounded" value="admin@gmail.com" />
+                    <label for="emailNname" class="mt-1 text-gray-500">Email or Username</label>
+                    <input placeholder="johndoe@gmail.com" name="emailNname" id="emailNname"
+                        class="p-2 w-full outline-none border-2 border-gray-200 rounded" value="admin@gmail.com" />
                 </div>
                 <div class="mb-2">
-                    <label class="mt-1 text-gray-500">Password</label>
-                    <input type="password" names="pswd" required placeholder="....." class="p-2 w-full outline-none border-2 border-gray-200 rounded" value="admin" />
+                    <label for="pswd" class="mt-1 text-gray-500">Password</label>
+                    <input type="password" name="pswd" id="pswd" required placeholder="....."
+                        class="p-2 w-full outline-none border-2 border-gray-200 rounded" value="admin" />
                 </div>
                 <div class="w-full mt-2">
-                    <button type="submit" class="bg-purple-700 text-white py-3 w-full text-center rounded-lg">Login</button>
+                    <button class="bg-purple-700 text-white py-3 w-full text-center rounded-lg">Login</button>
                 </div>
             </form>
         </div>
     </div>
-    
+
 </body>
+
 </html>
