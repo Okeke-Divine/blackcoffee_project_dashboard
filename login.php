b9e44e7b00b9feff4885c9b11b7be77b50a8c133
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 16:42:34 +0100

    Created the login ui

--- login.php
+++ login.php
@@ -6,6 +6,25 @@
 </head>
 <body>
 
+    <div class="bg-gray-200 w-full min-h-[100vh] flex justify-center items-center">
+        <div class="bg-white p-10 rounded-lg border-2 border-gray-300 w-[500px]">
+            <div class="font-bold text-2xl">Welcome to Dashboard!</div>
+            <div class="mt-1 text-gray-500">Please sign-in to continue</div>
+            <form class="mt-2">
+                <div class="mb-2">
+                    <label class="mt-1 text-gray-500">Email or Username</label>
+                    <input placeholder="johndoe@gmail.com" class="p-2 w-full outline-none border-2 border-gray-200 rounded" value="admin@gmail.com" />
+                </div>
+                <div class="mb-2">
+                    <label class="mt-1 text-gray-500">Password</label>
+                    <input type="password" required placeholder="....." class="p-2 w-full outline-none border-2 border-gray-200 rounded" value="admin" />
+                </div>
+                <div class="w-full mt-2">
+                    <button class="bg-blue-600 text-white py-3 w-full text-center rounded-lg">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
     
 </body>
 </html>
