57d763231ec2a2741e6efbb8bde60c5a6e9a7682
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 14:13:25 +0100

    Added table layout

---
+++ table-layout.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2024 at 03:12 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blackcoffee_project_dashboard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `topic` varchar(255) NOT NULL,
+  `pestle` varchar(255) NOT NULL,
+  `source` varchar(255) NOT NULL,
+  `insight` text NOT NULL,
+  `sector` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `region` varchar(255) NOT NULL,
+  `start_year` int(4) NOT NULL,
+  `end_year` int(4) NOT NULL,
+  `citylat` float NOT NULL,
+  `citylng` float NOT NULL,
+  `relevance` int(11) NOT NULL,
+  `likelihood` int(11) NOT NULL,
+  `impact` int(11) NOT NULL,
+  `swot` varchar(255) NOT NULL,
+  `intensity` int(11) NOT NULL,
+  `added` datetime NOT NULL,
+  `published` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
